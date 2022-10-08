af90a960cb881be1c608d78bb44e17f59719ba95
Kirill <k@kazakov.xyz>
2022-10-08 20:58:03 +0300

    App changed, to show GH actions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+         Hello from hackathon
+        </p>
+        <a
+            className="App-link"
+            href="https://it-onedevopschampion.ru/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          IT One DevOps Champion
+        </a>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
